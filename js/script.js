963bd71a58fcd44208d65cd66d3d79a7fc6bb450
Jason Mejia <mejiajason640@gmail.com>
2018-11-14 23:15:15 +0000

    rps else if

--- js/script.js
+++ js/script.js
@@ -4,13 +4,38 @@
 
 //GLOBAL VARIABLES
 /* global $ */
+var userChoice = "";
+
+var computerChoice = "";
+
+var winner = "";
 $("#shoot").click(function(){
+    userChoice = $("#input").val();
 
-$("#userChoice").text($("#input").val());
+    $("#userChoice").text(userChoice);
+        var randomNumber = Math.random();
+        if(randomNumber<.3333) {
+            
+            computerChoice = "rock";
+        } else if (randomNumber>.3333&& randomNumber<.6666) {
+            
+            computerChoice = "paper";
+        } else if (randomNumber>.6666&& randomNumber<1){
+            
+            computerChoice = "scissors";
+        }
+        $("#computerChoice").text(computerChoice);
+        
+        
+    
 });
-Var choice
 
 
+
+
+
+ 
+ 
 
 
 // DOCUMENT READY FUNCTION BELOW
